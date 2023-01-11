0ddcc10b407d9ddc5d4dbd1a49527a9af3054e0f
berkay.bilgic <berkay.bilgic@ozguryazilim.com.tr>
2023-01-11 15:50:24 +0300

    update tail recursion

--- Factorial/src/main/scala/Main.scala
+++ Factorial/src/main/scala/Main.scala
@@ -1,12 +1,17 @@
+import scala.annotation.tailrec
+
 object Main extends App {
 
-  def factorial(number :Int) : Int = {
-     number match {
-      case 0 => 1
-      case n => n * factorial(n-1)
+  def factorial(number: Int): Int = {
+    @tailrec
+    def fact(number: Int, acc: Int): Int = {
+      number match {
+        case 0 => acc
+        case n => fact(n - 1, n * acc)
+      }
     }
+    fact(number, 1)
   }
-  print(factorial(6))
 
-
+  print(factorial(0))
 }
